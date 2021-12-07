b46007492636d3239fe417d520d7bedf7cb78cb6
zhepama <zhepama@gmail.com>
2021-12-07 14:21:41 +0800

    删除上传文件的安全检查

--- assets/app.js
+++ assets/app.js
@@ -285,14 +285,7 @@
             upload: {
                 accept: 'image/*,.mp3, .wav, .rar',
                 url: '/api/upload/editor',
-                linkToImgUrl: '/api/upload/fetch',
-                filename(name) {
-                    return name.replace(/[^(a-zA-Z0-9\u4e00-\u9fa5\.)]/g, '').
-                        replace(/[\?\\/:|<>\*\[\]\(\)\$%\{\}@~]/g, '').
-                        replace('/\\s/g', '');
-                },
                 handler(files) {
-                    console.log(files);
                     vscode.postMessage({
                         type: 'upload',
                         files: files.map((f) => { return f.path; }),
